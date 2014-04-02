4b82709a4282eee37200a34a8161acc6889ef204
Joe Scott <joseph.scott@it.uu.se>
2014-04-02 11:12:06 +0200

    fixes memory mgmt/disposal issues
    - dfa is already a shared handle object, so now it is used correctly
    - distance[] is explicitly reallocated (from home) and copied
    - length is subscribed via Index advisor in the council (i=-1 to distinguish). advise() detects length-based failure, subsumption, fix, etc.
    - MAJOR bug fix: Advisors still in the council at dispose are explicitly unsubscribed. This wasn't necessary in the fixed-length version, because advisors were disposed only when their view became fixed (subscriptions to assigned views are automatically cancelled), and subsumption happened when all views were assigned. Rewriting the bounded version to the fixed length occurs when some views are not assigned; these views are disposed in dispose(), but they need to be explicitly cancelled so that the subscriptions don't get activated later.

--- src/proptest.cpp
+++ src/proptest.cpp
@@ -5,26 +5,31 @@
 
 using namespace Gecode;
 
-class Revenant : public Script {
+class Revenant : public Space {
 private:
   IntVarArray A;
   IntVar N;
 public:
+  enum {PROP_CLOSED, PROP_OPEN};
   Revenant(const SizeOptions& opt) : 
     A(*this, opt.size()*2, 1, 3),
     N(*this, 1, opt.size()*2) {
       REG lang1 = *(REG(IntArgs(3, 1,2,3))) + REG(1) + (REG(IntArgs(3, 1,2,3)))(opt.size() + 1,opt.size() + 1);
       DFA d1(lang1);
-      extensional(*this, A, d1, N);
-      //extensional(*this, A, d1);
       REG lang2 = *(REG(IntArgs(3, 1,2,3))) + REG(2) + (REG(IntArgs(3, 1,2,3)))(opt.size(),opt.size());
       DFA d2(lang2);
-      extensional(*this, A, d2, N);
-      //extensional(*this, A, d2);
       
-      branch(*this, A, INT_VAR_NONE(), INT_VAL_MIN(), &filter);
-      //branch(*this, A, INT_VAR_NONE(), INT_VAL_MIN());
-      branch(*this, N, INT_VAL_SPLIT_MIN());
+      switch(opt.propagation()) {
+        case PROP_CLOSED:
+          extensional(*this, A, d1);
+          extensional(*this, A, d2);
+          break;
+        case PROP_OPEN:
+          extensional(*this, A, d1, N);
+          extensional(*this, A, d2, N);
+          break;
+        default: break;
+      }
     }
   bool bounded(IntVar x, int i) const {
     return i <= N.min();
@@ -33,7 +38,7 @@ public:
     return static_cast<const Revenant&>(home).bounded(x,i);
   }
   Revenant(bool share, Revenant& s) :
-  Script(share,s) {
+  Space(share,s) {
     A.update(*this, share, s.A);
     N.update(*this, share, s.N);
   }
@@ -42,17 +47,33 @@ public:
   }
   virtual void print(std::ostream& os) const {
     os << N << ":";
-    for (int i = 0; i < N.min(); i++)
-      os << A[i];
-    os << std::endl;
+    // for (int i = 0; i < N.min(); i++)
+    //   os << A[i];
+    // os << std::endl;
+    os << A << std::endl;
   }
 };
 
 int main(int argc, char* argv[]) {
-  SizeOptions opt("Revenant");
+  SizeOptions opt("Single propagation iteration");
   opt.size(50);
   opt.solutions(0);
+  opt.propagation(Revenant::PROP_CLOSED,
+                  "closed", "use fixed-length extensional propagator");
+  opt.propagation(Revenant::PROP_OPEN,
+                  "open", "use bounded-length extensional propagator");
+  opt.propagation(Revenant::PROP_OPEN);
   opt.parse(argc,argv);
-  Script::run<Revenant,DFS,SizeOptions>(opt);
+  
+  Revenant* r = new Revenant(opt);
+  std::cout << "before : ";
+  r->print(std::cout);
+  std::cout << std::endl << " after : ";
+  SpaceStatus s = r->status();
+  r->print(std::cout);
+  if(s == SS_FAILED) {
+    std::cout << "\tFAILED!!";
+  }
+  std::cout << std::endl;
   return 0;
 }
